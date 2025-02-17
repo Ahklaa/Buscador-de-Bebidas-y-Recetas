3e5da68a648ea238a8a23c15a14faff5c2d14204
Ahkla <ahkla185@hotmail.com>
2025-02-17 10:15:13 -0500

    se creo el header

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+
+export default function Header() {
+  return (
+    <header className="bg-slate-900">
+        <div className="mx-auto container px-5 py-10">
+            <div className="flex justify-between items-center">
+                <img src="/logo.svg "className="w-32" alt="logo vino" />
+                <div>
+
+                </div>
+            </div>
+        </div>
+    </header>
+  )
+}
+
