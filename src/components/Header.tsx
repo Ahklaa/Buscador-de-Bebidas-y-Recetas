c1a29b027ac63e36a152b547faa50c2f953ab7d6
Ahkla <ahkla185@hotmail.com>
2025-02-17 10:34:50 -0500

    se agrego navegacion entre las paginas

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,22 @@
-
+import { Link, NavLink } from "react-router-dom"
 export default function Header() {
   return (
     <header className="bg-slate-900">
         <div className="mx-auto container px-5 py-10">
             <div className="flex justify-between items-center">
-                <img src="/logo.svg "className="w-32" alt="logo vino" />
                 <div>
-
+                    <img src="/logo.svg "className="w-22" alt="logo vino" />
                 </div>
+                <nav className="flex gap-5">
+                    <Link 
+                    to="/"
+                    className="uppercase text-white font-bold"
+                    >Inicio</Link>
+                    <Link 
+                    to="/Favoritos"
+                    className="uppercase text-white font-bold"
+                    >Favoritos</Link>
+                </nav>
             </div>
         </div>
     </header>
