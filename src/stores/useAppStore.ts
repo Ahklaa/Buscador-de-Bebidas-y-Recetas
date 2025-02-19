082e1c7e6048eb8fbfc410314a428fb24bc29335
Ahkla <ahkla185@hotmail.com>
2025-02-19 10:47:56 -0500

    se creo un archivo para el slices de favoritos, ademas se agrego en el useAppStore

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,7 +1,9 @@
 import {create} from'zustand'
 import { createRecipesSlices, RecipeSlicesType } from './recipeSlice'
+import { createFavoriteSlice, FavoriteSliceType } from './favoriteSlice'
 import { devtools } from 'zustand/middleware'
 
-export const useAppStore = create<RecipeSlicesType>()(devtools((...a) => ({
-    ...createRecipesSlices(...a)
+export const useAppStore = create<RecipeSlicesType & FavoriteSliceType>()(devtools((...a) => ({
+    ...createRecipesSlices(...a),
+    ...createFavoriteSlice(...a)
 })))
