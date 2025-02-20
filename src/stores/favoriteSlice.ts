dacf1c1d122f1fe90cc36ae599f1b42b851924d8
Ahkla <ahkla185@hotmail.com>
2025-02-20 15:01:46 -0500

    llamando el slice de notificacion para mostrarla cuando se crea o elimina de ffavoritos

--- src/stores/favoriteSlice.ts
+++ src/stores/favoriteSlice.ts
@@ -1,6 +1,7 @@
 import { Recipe } from "../types"
 import { StateCreator } from "zustand"
-import { createRecipesSlices, RecipeSlicesType } from "./recipeSlice"
+import { createRecipesSlices, RecipeSlicesType, } from "./recipeSlice"
+import { createNotificationSlice, NotificationSliceType } from "./notificationSlice"
 export type FavoriteSliceType = {
     favorites : Recipe[],
     handleClickFavorite : (recipe : Recipe) => void,
@@ -8,17 +9,27 @@ export type FavoriteSliceType = {
     loadFromStorage : () => void
 }
 
-export const createFavoriteSlice : StateCreator<FavoriteSliceType & RecipeSlicesType,[],[],FavoriteSliceType> = (set , get,api) => ({
+export const createFavoriteSlice : StateCreator<FavoriteSliceType & RecipeSlicesType & NotificationSliceType,[],[],FavoriteSliceType> = (set , get,api) => ({
     favorites : [],
     handleClickFavorite : (recipe) => {
         if(get().favoriteExist(recipe.idDrink)){
             set((state) =>({
                 favorites : state.favorites.filter(favorite => favorite.idDrink !== recipe.idDrink)
             }) )
+
+            createNotificationSlice(set,get,api).showNotification({
+                text : "Se ha eliminado el elemento de favoritos",
+                error : false
+            })
+
         }else{
             set((state) => ({
                 favorites : [...state.favorites, recipe]
             }))
+            createNotificationSlice(set,get,api).showNotification({
+                text : "Se ha agregado el elemento a favoritos",
+                error : false
+            })
         }
         createRecipesSlices(set,get,api).closeModal()
         localStorage.setItem("favorites",JSON.stringify(get().favorites))
--- src/stores/notificationSlice.ts
+++ src/stores/notificationSlice.ts
@@ -1,4 +1,6 @@
+import { set } from "zod";
 import { StateCreator } from "zustand";
+import { FavoriteSliceType } from "./favoriteSlice";
 
 export type Notification = {
     text : string,
@@ -7,13 +9,24 @@ export type Notification = {
 }
 
 export type NotificationSliceType = {
-    notification : Notification
+    notification : Notification,
+    showNotification : (payload : Omit<Notification, 'show'>) => void
 }
 
-export const createNotificationSlice : StateCreator<NotificationSliceType> = () => ({
+export const createNotificationSlice : StateCreator<NotificationSliceType & FavoriteSliceType, [] , [], NotificationSliceType> = (set,get) => ({
     notification : {
         text : "",
         error : false,
         show : false,
+    },
+
+    showNotification : (payload) => {
+        set({
+            notification : {
+                text : payload.text,
+                error : payload.error,
+                show : true
+            }
+        })
     }
 })
